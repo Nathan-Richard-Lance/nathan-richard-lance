93a7a91e5923a1c4d93e09dbc2e534b38bcba65c
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-05-30 11:43:51 -0500

    still working on the sql

--- database_design.sql
+++ database_design.sql
@@ -2,7 +2,15 @@ SELECT email_address
 FROM users
 WHERE users.unique_id = (
   SELECT ads.unique_id
+  FROM ads
   WHERE ads.unique_id =(
     SELECT ads.unique_id
-    FROM ads);
+    FROM ads));
 
+SELECT categories
+FROM catagories
+WHERE category.unique_id IN (
+  SELECT category.unique_id
+  FROM ad_category
+  WHERE ads.unique_id = ad.unique_id
+);
