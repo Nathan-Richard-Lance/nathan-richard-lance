5a8a97f1f2c7c02641c8baeb5340506160e16b6e
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-05-30 11:59:25 -0500

    finished the sql.

--- database_design.sql
+++ database_design.sql
@@ -14,3 +14,19 @@ WHERE category.unique_id IN (
   FROM ad_category
   WHERE ads.unique_id = ad.unique_id
 );
+
+SELECT *
+FROM ads
+WHERE ads.unique_id IN (
+  SELECT ads.unique_id
+  FROM ad_category
+  WHERE catagory.unique_id = ad.unique_id
+);
+
+SELECT *
+FROM ads
+WHERE ads.unique_id = (
+  SELECT ads.unique_id
+  FROM ads_users
+  WHERE users.unique_id = ads.unique_id
+);
